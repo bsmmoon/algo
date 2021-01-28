e8fdc5c63b950cc8d0d4b0796b8f7eb1b5b11722
bsmmoon <bsmmoon@gmail.com>
2021-01-28 23:59:35 +0900

    no need memo

--- Leetcode70.py
+++ Leetcode70.py
@@ -3,16 +3,15 @@
 
 class Solution(object):
     def climbStairs(self, n):
-        memo = { 0: 1 }
-        for i in range(1, n+1):
-            memo[i] = memo.get(i - 1, 0) + memo.get(i - 2, 0)
-        return memo[n]
+        prepre, pre = 0, 1
+        for i in range(1, n): prepre, pre = pre, pre + prepre
+        return pre + prepre
     
     def _climbStairs(self, n, memo={}):
         if n < 0: return 0
         if n == 0: return 1
         if memo.get(n): return memo[n]
-        memo[n] = self._climbStairs(n-1) + self._climbStairs(n-2)
+        memo[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
         return memo[n]
 
 class Test:
@@ -23,6 +22,7 @@ class Test:
 tests = [
     Test(2, 2),
     Test(3, 3),
+    Test(4, 5),
 ]
 
 solver = Solution()
